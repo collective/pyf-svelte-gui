1dd0411ea8a1553530cd710d65fd195aab73969d
MrTango <md@derico.de>
2020-10-23 16:13:29 +0300

    restructure searchform and filter, works withoput batching for now

--- src/stores.js
+++ src/stores.js
@@ -2,7 +2,6 @@ import { writable } from 'svelte/store';
 
 export const package_list = writable([]);
 
-export const plone_version = writable();
-export const package_type = writable();
+export const search_filter = writable({});
 export const search_term = writable("");
 export const search_classifiers = writable([]);
